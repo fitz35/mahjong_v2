ede276423da94630146e1bba25250c45a2e0d689
fitz <clement@lahoche.info>
2022-05-10 16:59:45 +0200

    hotfix image

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App bg-pageBase text-white h-screen">
       <header className='bg-pageDark items-center h-16 flex'>
-        <img src={logo} alt="Logo" />
+        <img className="h-16" src={logo} alt="Logo" />
         <h1 className="flex-1 object-left text-3xl font-bold underline">Mahjong</h1>
         <nav className='flex-1 object-right text-right text-3xl'>
           <NavLink className={({ isActive }) => isActive ? "bg-pageVeryDark" : "hover:bg-pageVeryDark"} to="/">Calculateur</NavLink> |{" "}
