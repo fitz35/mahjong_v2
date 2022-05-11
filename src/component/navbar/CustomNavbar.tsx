17a1bd098ef4261b80ff7ea608ed7e6b16f0c772
fitz <clement@lahoche.info>
2022-05-11 14:28:18 +0200

    adapt navbar

--- src/component/navbar/CustomNavbar.tsx
+++ src/component/navbar/CustomNavbar.tsx
@@ -12,11 +12,39 @@ export class CustomNavbar extends React.Component <{}, NavbarState>{
     }
 
     render() {
+        var td1 : string = "w-40 border-r border-white";
+        var td2 : string = "w-40 border-r border-white";
+        var td3 : string = "w-40";
+
+        switch(this.state.link){
+            case 1 :
+                td1 += " bg-pageVeryDark";
+                break;
+            case 2 :
+                td2 += " bg-pageVeryDark";
+                break;
+            case 3 :
+                td3 += " bg-pageVeryDark";
+                break;
+            default:
+
+        }
+
         return (
-            <nav className='flex-1 object-right text-right text-3xl'>
-                <NavLink className="hover:bg-pageVeryDark" to="/">Calculateur</NavLink> |{" "}
-                <NavLink className="hover:bg-pageVeryDark" to="/regles">Régles</NavLink> |{" "}
-                <NavLink className="hover:bg-pageVeryDark" to="/jouer">Jouer</NavLink>
+            <nav className='flex-1 h-16 object-right text-center text-3xl'>
+                <table className="h-16 ml-auto mr-0">
+                    <tr>
+                        <td className={td1}>
+                            <NavLink className="hover:bg-pageVeryDark" onClick={() => this.setState({link : 1})} to="/">Calculateur</NavLink>
+                        </td>
+                        <td className={td2}>
+                            <NavLink className="hover:bg-pageVeryDark" onClick={() => this.setState({link : 2})} to="/regles">Régles</NavLink>
+                        </td>
+                        <td className={td3}>
+                            <NavLink className="hover:bg-pageVeryDark" onClick={() => this.setState({link : 3})} to="/jouer">Jouer</NavLink>
+                        </td>
+                    </tr>
+                </table>                
             </nav>
         )
     }
